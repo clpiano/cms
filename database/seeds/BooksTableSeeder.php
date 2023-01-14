b0a087d4a8587df33357c0a87395af3349a4b046
clpiano <111512024+clpiano@users.noreply.github.com>
2023-01-14 23:58:18 +0900

    test

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -12,5 +12,14 @@ class BooksTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('books')->truncate();
+
+        $books = [
+                  ['name' => 'PHP Book',
+                   'price' => 2000,
+                   'author' => 'PHPER'],
+
+                  ];
+
     }
 }
