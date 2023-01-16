4da4d91322158b81cbfb11bcc7b7926263c458c8
clpiano <111512024+clpiano@users.noreply.github.com>
2023-01-16 17:58:51 +0900

    test

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -18,8 +18,17 @@ class BooksTableSeeder extends Seeder
                   ['name' => 'PHP Book',
                    'price' => 2000,
                    'author' => 'PHPER'],
-
+                  ['name' => 'Laravel Book',
+                   'price' => 3000,
+                   'author' => null],
+                  ['name' => 'Ruby Book',
+                   'price' => 2500,
+                   'author' => 'Rubyist']
                   ];
 
+    foreach($books as $book) {
+      \App\Book::create($book);
+
+    }
     }
 }
